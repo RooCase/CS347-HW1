6f9aac608b78a32a409cd23236cd77912ddd7c69
JWin23 <iamkaungthantwin@gmail.com>
2023-09-14 19:45:37 -0500

    John's initial commit with has_won function

---
+++ board.py
@@ -0,0 +1,9 @@
+def has_won(capturedX, capturedO):
+    if capturedX >= 10:
+        return print("X has won!")
+    elif capturedO >= 10:
+        return print("O has won!")
+    #check if five in a row
+
+
+    
