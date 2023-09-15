fe13c8997b375ef5d7a51d76b8e823a65648e372
Roo Case <roo@roocase.org>
2023-09-14 23:14:12 -0500

    Updated main code, added new game functionality, and fixed a typo with "y" vs "O" in board.py

--- board.py
+++ board.py
@@ -4,12 +4,12 @@ class Board:
     def __init__(self):
         boardState = "-------------------"*19
         capturedX = 0
-        capturedY = 0
+        capturedO = 0
 
     def updatePoints(self, player):
         if player == "x":
             self.capturedX += 2
-        elif player == "y":
+        elif player == "o":
             self.capturedy += 2
         else:
             raise Exception("Invalid playerID provided")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import board
+from flask import Flask
 from random import randint
 
+app = Flask(__name__)
 gameDict = {}
 
 class game:
@@ -9,3 +11,12 @@ class game:
         gameBoard = board()
         playerState = "x"
         gameDict.update(gameID, self)
+
+@app.route('/newgame/player')
+def newGame():
+    activeGame = game()
+    return {
+        'id': activeGame.gameID,
+        'state': activeGame.playerState
+    }
+
