3b4de85ebcd0ed703b349d04f02de5760e7af6c3
JWin23 <iamkaungthantwin@gmail.com>
2023-09-14 20:41:30 -0500

    transferred has_won into a method and changed class name into Board for syntax

--- board.py
+++ board.py
@@ -1,18 +1,10 @@
-def has_won(capturedX, capturedO):
-    if capturedX >= 10:
-        return print("X has won!")
-    elif capturedO >= 10:
-        return print("O has won!")
-    #check if five in a row
-
-
-    
-class board:
+#gamestate is in the format of player#board#capturedX#capturedO
+ 
+class Board:
     def __init__(self):
         boardState = "-------------------"*19
         capturedX = 0
         capturedY = 0
-
 
     def updatePoints(self, player):
         if player == "x":
@@ -21,3 +13,10 @@ class board:
             self.capturedy += 2
         else:
             raise Exception("Invalid playerID provided")
+    
+    def has_won(capturedX, capturedO):
+        if capturedX >= 10:
+            return print("X has won!")
+        elif capturedO >= 10:
+            return print("O has won!")
+        #check if five in a row
