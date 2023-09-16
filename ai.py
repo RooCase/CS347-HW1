301cf2a9b95ba0b3021111cfc382f9bef8a60655
Artem Yushko <yushko2002@gmail.com>
2023-09-16 10:51:43 -0500

    got AI done

---
+++ ai.py
@@ -0,0 +1,21 @@
+"""
+Class for AI that makes moves opposite to the player.
+"""
+
+import random
+
+class Ai(object):
+    def __init__(self, currboard):
+        self.player = ""
+        self.board = currboard
+
+    def move(self):
+        """
+        Calls a Board function to make a move as an AI
+        """
+        while True: # generates a random position until it encounters an empty one
+            x, y = random.random(0, 18), random.random(0, 18)
+            if self.board.pos_is_empty(x + (19 * y) - 1):
+                break
+        self.board.make_move(self.player, x, y) # calls the board to make a move
+        return
