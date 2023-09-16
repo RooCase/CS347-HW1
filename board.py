e4bdda458c279c8a2e1c090df105c8bdb5b6bd9f
JWin23 <iamkaungthantwin@gmail.com>
2023-09-16 10:21:30 -0500

    fixed wrong position calculation in board.py

--- board.py
+++ board.py
@@ -2,10 +2,10 @@
  
 class Board:
     def __init__(self):
-        next_player = ""
-        boardState = "-------------------"*19
-        capturedX = 0
-        capturedO = 0
+        self.next_player = ""
+        self.boardState = "-------------------"*19
+        self.capturedX = 0
+        self.capturedO = 0
 
     def update_points(self, player):
         if player == "X":
@@ -21,28 +21,33 @@ class Board:
         else:
             self.next_player = "X"
     
-    def has_won(capturedX, capturedO, playerX, playerY):
+    def has_won(self, capturedX, capturedO, player, playerX, playerY):
         if capturedX >= 10:
             return print("X has won!")
         elif capturedO >= 10:
             return print("O has won!")
         else:
-            return self.is_five_in_row(playerX, playerY)
+            return self.is_five_in_row(player, playerX, playerY)
         
-    def is_five_in_row(self, playerX, playerY):
-        pos = playerX + (19 * playerY)
+    def is_five_in_row(self, player, playerX, playerY):
+        pos = playerX + (19 * playerY) - 1 #-1 to adjust for indexing
         dir_dic = [[1,0], [-1,0], [0,1], [0,-1], [1,1], [-1,-1], [1,-1], [-1,1]]
         for dir in dir_dic:
-            return self.check_five_in_row_in_dir(pos, 1, dir[0], dir[1])
+            has_won = self.check_five_in_row_in_dir(player, pos, 1, dir[0], dir[1])
+            if has_won:
+                return has_won
+        return has_won
     
     # n is the nth row on the board where 0 <= n <= 8
     # dirX and dirY are the direction in which 5-in-a-row is formed. Their values: -1, 0, 1
-    def check_five_in_row_in_dir(pos, n, dirX, dirY):
-        nxt = pos + (dirX * 19 * n) + (dirY * n)
+    def check_five_in_row_in_dir(self, player, pos, n, dirX, dirY):
+        nxt = pos + (dirX * 19 * n) + (dirY * n) - 1 #-1 to adjust for indexing
         if nxt > 361:
+            return False
+        if self.boardState[nxt] != player:
             return False
         if n == 4:
             return True
         else:
-            return self.check_five_in_row_in_dir(nxt, n+1)
+            return self.check_five_in_row_in_dir(player, nxt, n+1)
     
